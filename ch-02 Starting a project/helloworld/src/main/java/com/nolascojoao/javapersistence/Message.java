b220558c26176da8bb700367413311bb4d7975c3
João Pedro Nolasco <nolascojoaops@gmail.com>
2024-04-14 10:39:36 -0300

    ch-02: add Message class to persist data with hibernate mapping

---
+++ ch-02 Starting a project/helloworld/src/main/java/com/nolascojoao/javapersistence/Message.java
@@ -0,0 +1,28 @@
+package com.nolascojoao.javapersistence;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Message {
+
+    public Message() {
+
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    
+}
