ae844bb76f2928b9c56f50084c0985dfbcf45537
João Pedro Nolasco <nolascojoaops@gmail.com>
2024-04-14 22:39:03 -0300

    ch-02: update HelloWorldJPATest class with comments for further study

--- ch-02 Starting a project/helloworld/src/test/java/com/nolascojoao/javapersistence/HelloWorldJPATest.java
+++ ch-02 Starting a project/helloworld/src/test/java/com/nolascojoao/javapersistence/HelloWorldJPATest.java
@@ -14,38 +14,51 @@ public class HelloWorldJPATest {
 
     @Test
     public void storeAndLoadMessage() {
+        // Create an EntityManagerFactory based on the persistence unit ch02
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("ch02");
 
         try {
+            // Create an EntityManager from the EntityManagerFactory
             EntityManager em = emf.createEntityManager();
+            // Begin a transaction
             em.getTransaction().begin();
 
+            // Create a new instance of Message and set its text to "Hello World"
             Message message = new Message();
             message.setText("Hello World");
 
+            // Persist the message to the database
             em.persist(message);
 
+            // Commit the transaction
+            // INSERT into MESSAGE(ID, TEXT) values(1,"Hello World")
             em.getTransaction().commit();
-            //INSERT into MESSAGE(ID, TEXT) values(1,'Hellow Wowrld')
 
+            // Begin another transaction
             em.getTransaction().begin();
 
+            // Execute a JPQL query to select all messages
+            // SELECT * from MESSAGE
             List<Message> messages = em.createQuery("select m from Message m", Message.class)
                     .getResultList();
-            //SELECT * from MESSAGE
 
+            // Update the text of the last message in the list
             messages.get(messages.size() - 1).setText("Hello World from JPA!");
 
+            // Commit the second transaction
+            // UPDATE MESSAGE set TEXT = 'Hello World from JPA!' where ID = 1
             em.getTransaction().commit();
-            //UPDATE MESSAGE set TEXT = 'Hello World from JPA!' where ID = 1
 
+            // Verify the results using JUnit
             assertAll(
                     () -> assertEquals(1, messages.size()),
                     () -> assertEquals("Hello World from JPA!", messages.get(0).getText())
             );
 
+            // Close the EntityManager
             em.close();
         } finally {
+            // Close the EntityManagerFactory
             emf.close();
         }
     }
