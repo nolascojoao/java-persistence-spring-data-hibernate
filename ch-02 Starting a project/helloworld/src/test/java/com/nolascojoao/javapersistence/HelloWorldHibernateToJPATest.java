ff9c0db4aae08b7962039ed3df49a25f3bbe928f
João Pedro Nolasco <nolascojoaops@gmail.com>
2024-04-15 18:48:27 -0300

    ch-02: implement persistent test unwrapping Hibernate to JPA

--- ch-02 Starting a project/helloworld/src/test/java/com/nolascojoao/javapersistence/HelloWorldHibernateToJPATest.java
+++ ch-02 Starting a project/helloworld/src/test/java/com/nolascojoao/javapersistence/HelloWorldHibernateToJPATest.java
@@ -1,12 +1,18 @@
 package com.nolascojoao.javapersistence;
 
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import org.hibernate.cfg.Configuration;
+import org.junit.jupiter.api.Test;
 
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HelloWorldHibernateToJPATest {
 
@@ -21,6 +27,35 @@ public class HelloWorldHibernateToJPATest {
             properties.put(element, configuration.getProperties().getProperty(element));
         }
         return Persistence.createEntityManagerFactory("ch02", properties);
+    }
+
+    @Test
+    public void shouldStoreAndLoadMessage() {
+
+        EntityManagerFactory emf = createEntityManagerFactory();
+
+        try {
+            EntityManager em = emf.createEntityManager();
+            em.getTransaction().begin();
+
+            Message message = new Message();
+            message.setText("Hello World from Hibernate to JPA!");
+
+            em.persist(message);
+
+            em.getTransaction().commit();
+
+            List<Message> messages = em.createQuery("select m from Message m", Message.class).getResultList();
+
+            assertAll(
+                    () -> assertEquals(1, messages.size()),
+                    () -> assertEquals("Hello World from Hibernate to JPA!", messages.get(0).getText())
+            );
+
+            em.close();
+        } finally {
+            emf.close();
+        }
     }
 
 }
